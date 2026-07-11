4749785b18ec8aed76f7cd767bb11f1521b3cd62
Somoza14 <somoza140205@gmail.com>
2026-07-11 01:54:19 -0600

    ACTUALIZACION LOCALHOST

--- server/server.js
+++ server/server.js
@@ -368,7 +368,6 @@ app.use(express.static("../"));
 // ============================================================
 // //  ESTO FUNCIONA EN TU PC Y EN RAILWAY
 const PORT = process.env.PORT || "http://localhost:4000";
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.listen(4000, () => {
+  console.log("Servidor SQLite ejecutándose en http://localhost:4000");
 });
